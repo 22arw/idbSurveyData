4381b39be66e901aeb7dd41abf2c20db9dd23767
Kevin Falting <kevinfalting@gmail.com>
2018-03-22 14:16:31 -0500

    a

--- lambdaFunctions/typeform.js
+++ lambdaFunctions/typeform.js
@@ -45,7 +45,7 @@ exports.handler = function(event, context, callback) {
 
         callback(null, {
             statusCode: 200,
-            body: data
+            body: rawData
         });
 
       }).catch(console.error)
