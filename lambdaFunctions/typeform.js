e9afd50d7d30b53e3fe69024958b05dd14eec4d5
Kevin Falting <kevinfalting@gmail.com>
2018-03-22 12:20:04 -0500

    a

--- lambdaFunctions/typeform.js
+++ lambdaFunctions/typeform.js
@@ -1,4 +1,25 @@
+const axios = require('axios');
+
 exports.handler = function(event, context, callback) {
+
+    baseURL = 'https://api.typeform.com/';
+    formID = 'ue4FsQ';
+    accessToken = 'CuAPnXc9Lb5KuqdqPLbTETRmitNHXEP6tUV8VWW38HqA';
+
+    typeformURL = baseURL + 'forms/' + formID + '/responses';
+
+    axios.get(typeformURL)
+        .then(function(response) {
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+        }).catch(error => {
+            console.log(error);
+        });
+   
+
     callback(null, {
         statusCode: 200,
         body: "Hello, World"
