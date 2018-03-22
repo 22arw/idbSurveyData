b184aad30b16679135403c723ed4f5881c43604d
Kevin Falting <kevinfalting@gmail.com>
2018-03-22 14:19:34 -0500

    a

--- js/callTypeformLambda.js
+++ js/callTypeformLambda.js
@@ -1,22 +1,30 @@
 $(function() {
 
     async function getData(url) {
-        const dataset = await $.ajax(url);
+        const dataset = await $.ajax({
+            type: 'GET',
+            url: url,
+            data: {},
+            success: function(resp){
+                data = resp;
+                success(resp);
+            }
+          });
         success(dataset);
     }
 
     getData('/.netlify/functions/typeform');
 
 
-    // $.ajax({
-    //     type: 'GET',
-    //     url: '/.netlify/functions/typeform',
-    //     data: {},
-    //     success: function(resp){
-    //         data = resp;
-    //         success(resp);
-    //     }
-    //   });
+    $.ajax({
+        type: 'GET',
+        url: '/.netlify/functions/typeform',
+        data: {},
+        success: function(resp){
+            data = resp;
+            success(resp);
+        }
+      });
 
       function success(resp) {
           document.getElementById('data').innerText = resp;
