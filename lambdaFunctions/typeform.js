389423a45cf97daa7c06630d26e62c586f82588f
Kevin Falting <kevinfalting@gmail.com>
2018-04-11 19:14:58 -0500

    solves iss #1

--- lambdaFunctions/typeform.js
+++ lambdaFunctions/typeform.js
@@ -4,15 +4,16 @@ var baseURL = 'https://api.typeform.com/';
 var formID = 'ue4FsQ';
 var accessToken = 'CuAPnXc9Lb5KuqdqPLbTETRmitNHXEP6tUV8VWW38HqA';
 var page_size = 'page_size=1000';
+var typeformURL;
 
-var typeformURL = baseURL + 'forms/' + formID + '/responses' + '?' + page_size + '&completed=true';
+const typeformBaseURL = baseURL + 'forms/' + formID + '/responses?' + page_size + '&completed=true';
 
 exports.handler = function(event, context, callback) {
 
   if (event.queryStringParameters.since != undefined && event.queryStringParameters.until != undefined) {
     let since = event.queryStringParameters.since;
     let until = event.queryStringParameters.until;
-    typeformURL += "&since=" + since + "&until=" + until;
+    typeformURL += typeformBaseURL + "&since=" + since + "&until=" + until;
   }
 
   console.log(typeformURL);
@@ -42,7 +43,6 @@ exports.handler = function(event, context, callback) {
 
   getUrl(typeformURL).then((rawData) => { 
     const data = JSON.parse(rawData)
-    // console.log(data)
 
     callback(null, {
         statusCode: 200,
